786514c56339e50b22b517a00510a233508957bf
Nicholas Dechert <dechert.nicholas@gmail.com>
2022-02-28 19:42:34 -0800

    First attempts at rotation

--- Assets/CubeMover.cs
+++ Assets/CubeMover.cs
@@ -7,15 +7,28 @@ public class CubeMover : MonoBehaviour
     [Range(0, 100)]
     public int XForce;
 
+    public Transform FrontBogieTarget;
+    public Transform BackBogieTarget;
+
+    public Transform FrontBogieTransform;
+    public Transform BackBogieTransform;
+
+    public Transform FrontBogieGoal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //FrontBogieTransform.position = FrontBogieGoal.position;
+
+        Vector3 direction = FrontBogieTransform.position - BackBogieTarget.position;
+        transform.rotation = Quaternion.FromToRotation(Vector3.right, direction);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += new Vector3(XForce * Time.deltaTime, 0, 0);
+
     }
 }
